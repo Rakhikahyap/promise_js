fed00a7797034da6669d32d69ee3189952dcb6f5
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-11-18 23:27:37 +0530

    Add files via upload

---
+++ concat.js
@@ -0,0 +1,9 @@
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = [1, 2, 3];
+// const arr3 = arr1.concat(arr2);
+// console.log(arr1,arr2,arr3)
+
+const arr1 = [1, 2, [3, 4]];
+const arr2 = [[5, 6], 7, 8];
+const arr3 = arr1.concat(arr2);
+console.log(arr1,arr3,arr2)
